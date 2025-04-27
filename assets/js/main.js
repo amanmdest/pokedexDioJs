b91ec184f8a19ecbeff926af89ef3aa85042e729
amanmdest <amanmdest@gmail.com>
2025-04-27 05:46:47 -0300

    Primeira requisição: listagem dinâmica no html com PokeAPI.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,40 @@
 // btnCadabra.addEventListener("click", function() {
 //     console.log('pudim')
 // })
+
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}?limit=${limit}`
+
+
+function convertPokemonToLi(pokemon, pokemonId) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail ">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/other/dream-world/${pokemonId}.svg" alt=${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonOl = document.getElementById('pokemonList')
+// console.log(pokemonOl)
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => { 
+        for (let i = 0; i < pokemonList.length; i++) {
+            const pokemon = pokemonList[i];
+            const id = i+1
+            const pokemonLi = convertPokemonToLi(pokemon, id)
+            pokemonOl.innerHTML += pokemonLi
+        }
+    })
+    .catch((error) => console.error(error))
