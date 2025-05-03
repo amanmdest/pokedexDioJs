7bbb2453ec61c1ec867fd80462e3f27a12ff365b
amanmdest <amanmdest@gmail.com>
2025-05-03 16:21:46 -0300

    Feat: mecanismo de paginação, simplificando estrutura de chamada da API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,37 @@
-const pokemonOl = document.getElementById('pokemonList')
-// console.log(pokemonOl)
+const pokemonOl = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
+let offset = 0;
+const limit = 5;
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}"> 
-            <span class="number">${pokemon.number}</span> 
-            <span class="name">${pokemon.name}</span> 
-            <div class="detail "> 
-                <ol class="types"> 
-                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
-                </ol>
-                <img src="${pokemon.photo}" alt=${pokemon.name}">
-            </div>
-        </li>
-    `
-}
+function loadPokemonItems(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemonList = []) => {
+    const newHtml = pokemonList
+      .map(
+        (pokemon) => `
+              <li class="pokemon ${pokemon.type}"> 
+                  <span class="number">${pokemon.number}</span> 
+                  <span class="name">${pokemon.name}</span> 
+                  <div class="detail "> 
+                      <ol class="types"> 
+                          ${pokemon.types
+                            .map(
+                              (type) => `<li class="type ${type}">${type}</li>`
+                            )
+                            .join("")}
+                      </ol>
+                      <img src="${pokemon.photo}" alt=${pokemon.name}">
+                  </div>
+              </li>
+          `
+      )
+      .join("");
+    pokemonOl.innerHTML += newHtml;
+  });
+}  
 
-pokeApi.getPokemons().then((pokemonList = []) => { 
-    pokemonOl.innerHTML = pokemonList.map(convertPokemonToLi).join('')
-})
-    
+loadPokemonItems(offset, limit);
+
+loadMoreButton.addEventListener("click", () => {
+  offset += limit;
+  loadPokemonItems(offset, limit);
+});
