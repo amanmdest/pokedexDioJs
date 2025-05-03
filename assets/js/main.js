daf59e5e6f7e7a1731b93849d87a7ea39fe64b97
amanmdest <amanmdest@gmail.com>
2025-05-03 12:39:28 -0300

    Feat: pokemon-model para facilitar integração com pokeApi, tipos dinamicos com background-color

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,20 @@
-// const btnCadabra = document.getElementById('btnCadabra')
-// btnCadabra.addEventListener("click", function() {
-//     console.log('pudim')
-// })
+const pokemonOl = document.getElementById('pokemonList')
+// console.log(pokemonOl)
 
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}?limit=${limit}`
-
-
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
-function convertPokemonToLi(pokemon, pokemonId) {
+function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon">
-            <span class="number">${pokemon.order}</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detail ">
-                <ol class="types">
-                    ${convertPokemonTypesToLi(pokemon.types).join(' ')}
+        <li class="pokemon ${pokemon.type}"> 
+            <span class="number">${pokemon.number}</span> 
+            <span class="name">${pokemon.name}</span> 
+            <div class="detail "> 
+                <ol class="types"> 
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                 </ol>
-                <img src="${pokemon.sprites.other.dream_world.front_default}" alt=${pokemon.name}">
+                <img src="${pokemon.photo}" alt=${pokemon.name}">
             </div>
         </li>
     `
 }
-
-const pokemonOl = document.getElementById('pokemonList')
-// console.log(pokemonOl)
 
 pokeApi.getPokemons().then((pokemonList = []) => { 
     pokemonOl.innerHTML = pokemonList.map(convertPokemonToLi).join('')
